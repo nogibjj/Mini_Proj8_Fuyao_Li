4944315b8fdee74146b2fdd740fcc2f495b0372e
fuayo li <fuyao.li@duke.edu>
2024-10-27 10:20:49 -0400

    Delete Local variable `duration`

--- test_main.py
+++ test_main.py
@@ -2,6 +2,6 @@ import os
 from main import benchmark_file_io
 
 def test_benchmark_file_io():
-    duration = benchmark_file_io()
+    benchmark_file_io()
     
     assert os.path.exists("data_sample.txt"), "Output file was not created"
